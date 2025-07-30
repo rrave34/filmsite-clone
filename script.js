336e7486703604e87cb28312ff583bd6702c2e47
rrave34 <znginefe@gmail.com>
2025-07-30 16:21:57 +0300

    How To work kısmı eklendi.

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-fetch("menu.json")
-    .then(response => response.json())
-    .then(data => {
-        data.menu.forEach(item => {
-            console.log(item.name);
-            {
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('menu.json')
+        .then(res => res.json())
+        .then(navItems => {
+            const navMenu = document.getElementById("nav-menu");
 
-            }
+            navItems.forEach(item => {
+                const li = document.createElement("li");
+                const a = document.createElement("a");
+                a.href = item.link;
+                a.textContent = item.name;
+                if (item.class) a.classList.add(item.class);
+                li.appendChild(a);
+                navMenu.appendChild(li);
+            });
+        })
+        .catch(error => {
+            console.error("Menü yüklenemedi:", error);
         });
-
-        document.getElementById("loginBtn").textContent = data.buttons.login;
-        document.getElementById("registerBtn").textContent = data.buttons.register;
-    })
-    .catch(err => console.error(err));
+});
